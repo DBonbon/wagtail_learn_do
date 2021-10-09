c43f5906863d47d5bf340f03f13fb4cc076d5e0e
Yariv Friedman <yrfriedman@gmail.com>
2021-10-09 14:41:17 +0200

    initial commit

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,6 +1,8 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
+SECRET_KEY = 'django-insecure-v_2h85gz3p&=*kxg6y=revgb085*_p^dce=e%rh^8h1cxv+30!'
+ALLOWED_HOSTS = ['*']
 
 try:
     from .local import *
